cd10d656a3158b7ce3508ff3b8106b7a2d596047
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-03-10 23:16:41 +0900

    [express-rest-api-practice]feat: add middleware to root route and add greetMiddleware to greet routes

--- express-rest-api-practice/server.js
+++ express-rest-api-practice/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("This is express study");
+    next();
+  },
+  (req, res) => {
+    res.status(200).send("hello world");
+  }
+);
+
+const greetMiddleware = (req, res, next) => {
+  console.log("This is greet route.");
+  next();
+};
+
+app.get("/greet", greetMiddleware, (req, res) => {
+  res.status(200).send("Welcome!");
 });
 
-app.get("/greet/:username", (req, res) => {
+app.get("/greet/:username", greetMiddleware, (req, res) => {
   res.status(200).send(`hello, ${req.params.username}`);
 });
 
