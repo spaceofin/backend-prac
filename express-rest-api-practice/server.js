7afd6928b8e801770d90ffe636be1a8465e16adf
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-03-11 12:03:51 +0900

    [express-rest-api-practice]feat: add error-triggering routes(/error, /error-throw, /not-found) and error handlers

--- express-rest-api-practice/server.js
+++ express-rest-api-practice/server.js
@@ -28,3 +28,35 @@ app.get("/greet/:username", greetMiddleware, (req, res) => {
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+const errorOccurMiddleware = (req, res, next) => {
+  console.log("error");
+  next(new Error("Error Occurred"));
+};
+
+app.get("/error", errorOccurMiddleware, (req, res) => {
+  res.status(200).send("This is error page");
+});
+
+app.get("/error-throw", (req, res) => {
+  throw new Error("Error Thrown");
+});
+
+app.get("/not-found", (req, res, next) => {
+  const error = new Error("Sometihne went wrong!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send("Page not found");
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Internal Server Error");
+});
