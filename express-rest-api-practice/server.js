471377c072049a06ab109fbf8af614dccc89f67c
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-03-12 23:18:25 +0900

    [express-rest-api-practice]feat: add /greets/all and /greets/all/stream routes to fetch and send all greet messages using mget and scanStream respectively

--- express-rest-api-practice/server.js
+++ express-rest-api-practice/server.js
@@ -11,17 +11,31 @@ const redis = new Redis({
 
 const init = async () => {
   await redis.flushdb();
-  await Promise.all([
-    redis.set(
-      "greets:1",
-      JSON.stringify({ id: 1, message: "Hello! Welcome!" })
-    ),
-    redis.set("greets:2", JSON.stringify({ id: 2, message: "Good Day~" })),
-    redis.set(
-      "greets:3",
-      JSON.stringify({ id: 3, message: "Have a nice day :D" })
-    ),
-  ]);
+  await redis.mset(
+    "greets:1",
+    JSON.stringify({ id: 1, message: "Hello! Welcome!" }),
+    "greets:2",
+    JSON.stringify({ id: 2, message: "Good Day~" }),
+    "greets:3",
+    JSON.stringify({ id: 3, message: "Have a nice day :D" }),
+    "greets:4",
+    JSON.stringify({
+      id: 4,
+      message: "Seize the day with joy and passion!",
+    }),
+    "greets:5",
+    JSON.stringify({ id: 5, message: "Greetings from the sunny side!" }),
+    "greets:6",
+    JSON.stringify({ id: 6, message: "Wishing you all the best today!" }),
+    "greets:7",
+    JSON.stringify({ id: 7, message: "Enjoy every moment of today!" }),
+    "greets:8",
+    JSON.stringify({ id: 8, message: "Smile and have a wonderful day!" }),
+    "greets:9",
+    JSON.stringify({ id: 9, message: "Make today amazing!" }),
+    "greets:10",
+    JSON.stringify({ id: 10, message: "Stay positive and keep shining!" })
+  );
 };
 
 app.get(
@@ -42,6 +56,42 @@ const greetMiddleware = (req, res, next) => {
 
 app.get("/greet", greetMiddleware, (req, res) => {
   res.status(200).send("Welcome!");
+});
+
+app.get("/greets/all", async (req, res) => {
+  try {
+    const keys = await redis.keys("greets:[0-9]*");
+    const greets = await redis.mget(keys);
+    const parsedGreets = greets.map((greet) => JSON.parse(greet));
+    parsedGreets.sort((a, b) => a.id - b.id);
+    res.status(200).send(parsedGreets);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal error");
+  }
+});
+
+app.get("/greets/all/stream", async (req, res) => {
+  try {
+    const stream = redis.scanStream({
+      match: "greets:[0-9]*",
+      count: 2,
+    });
+
+    const greets = [];
+    for await (const resultKeys of stream) {
+      for (const key of resultKeys) {
+        const value = await redis.get(key);
+        const greet = JSON.parse(value);
+        greets.push(greet);
+      }
+    }
+    greets.sort((a, b) => a.id - b.id);
+    res.status(200).send(greets);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal error");
+  }
 });
 
 app.get("/greet/:id", greetMiddleware, async (req, res) => {
